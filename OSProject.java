f0953bfb47906b02438d91d9d4368e571bb7e391
mickmichelman-a11y <mickmichelman@gmail.com>
2026-04-19 23:03:44 -0400

    Update OSProject.java

--- OSProject.java
+++ OSProject.java
@@ -9,15 +9,19 @@ public class OSProject {
     public static void main(String[] args) {
         List<Process> processes = new ArrayList<>();
         
-        //adding processes
+        //adding processes 
         processes.add(new Process(1,0,7,2,3));
         processes.add(new Process(2,1,5,2,1));
         processes.add(new Process(3,2,8,3,5));
         processes.add(new Process(4,3,10,5,1));
         processes.add(new Process(5,4,2,1,1));
         
-        FCFS fcfsSchedule = new FCFS(processes);
-        fcfsSchedule.Scheduling();
+        System.out.println("-----FCFS-----");
+        Algorithm schedule = new FCFS(processes);
+        schedule.Scheduling();
+        System.out.println("-----RoundRobin-----");
+        schedule = new RoundRobin(processes, 4);
+        schedule.Scheduling();
         
     }
     
