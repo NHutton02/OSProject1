b82e7f8e1bc2210301c7c9afc7d749626e11c897
mickmichelman-a11y <mickmichelman@gmail.com>
2026-05-16 22:30:09 -0400

    Update OSProject.java
    this has the new test cases that he had us test during the presentation

--- os/project/OSProject.java
+++ os/project/OSProject.java
@@ -11,8 +11,8 @@ public class OSProject {
         List<Process> priorityProcesses = new ArrayList<>();
         
         // id, arrival time, burst time, priority, io start, io duration
-               fcfsProcesses.add(new Process(1, 0, 7, 2, 3, 2));
-        fcfsProcesses.add(new Process(2, 1, 4, 1, 0, 0));
+        /*fcfsProcesses.add(new Process(1, 0, 7, 0, 2, 2));
+        fcfsProcesses.add(new Process(2, 3, 4, 0, 0, 2));
         fcfsProcesses.add(new Process(3, 2, 9, 4, 4, 3));
         fcfsProcesses.add(new Process(4, 3, 5, 2, 0, 0));
         fcfsProcesses.add(new Process(5, 4, 3, 1, 1, 2));
@@ -30,12 +30,12 @@ public class OSProject {
         fcfsProcesses.add(new Process(17, 16, 2, 1, 0, 0));
         fcfsProcesses.add(new Process(18, 17, 11, 5, 6, 3));
         fcfsProcesses.add(new Process(19, 18, 4, 2, 0, 0));
-        fcfsProcesses.add(new Process(20, 19, 7, 3, 3, 1));
+        fcfsProcesses.add(new Process(20, 19, 7, 3, 3, 1));*/
 
-        rrProcesses.add(new Process(1, 0, 7, 2, 3, 2));
-        rrProcesses.add(new Process(2, 1, 4, 1, 0, 0));
-        rrProcesses.add(new Process(3, 2, 9, 4, 4, 3));
-        rrProcesses.add(new Process(4, 3, 5, 2, 0, 0));
+        /*rrProcesses.add(new Process(1, 0, 20, 0, 2, 1));
+        rrProcesses.add(new Process(2, 3, 7, 0, 0, 0));
+        rrProcesses.add(new Process(3, 5, 2, 0, 0, 0));
+        rrProcesses.add(new Process(4, 7, 3, 0, 0, 0));
         rrProcesses.add(new Process(5, 4, 3, 1, 1, 2));
         rrProcesses.add(new Process(6, 5, 8, 5, 0, 0));
         rrProcesses.add(new Process(7, 6, 2, 3, 0, 0));
@@ -51,11 +51,11 @@ public class OSProject {
         rrProcesses.add(new Process(17, 16, 2, 1, 0, 0));
         rrProcesses.add(new Process(18, 17, 11, 5, 6, 3));
         rrProcesses.add(new Process(19, 18, 4, 2, 0, 0));
-        rrProcesses.add(new Process(20, 19, 7, 3, 3, 1));
+        rrProcesses.add(new Process(20, 19, 7, 3, 3, 1));*/
 
-        priorityProcesses.add(new Process(1, 0, 7, 2, 3, 2));
-        priorityProcesses.add(new Process(2, 1, 4, 1, 0, 0));
-        priorityProcesses.add(new Process(3, 2, 9, 4, 4, 3));
+        //priorityProcesses.add(new Process(1, 0, 7, 2, 3, 2));
+        //priorityProcesses.add(new Process(2, 1, 4, 1, 0, 0));
+        /*priorityProcesses.add(new Process(3, 2, 9, 4, 4, 3));
         priorityProcesses.add(new Process(4, 3, 5, 2, 0, 0));
         priorityProcesses.add(new Process(5, 4, 3, 1, 1, 2));
         priorityProcesses.add(new Process(6, 5, 8, 5, 0, 0));
@@ -72,18 +72,18 @@ public class OSProject {
         priorityProcesses.add(new Process(17, 16, 2, 1, 0, 0));
         priorityProcesses.add(new Process(18, 17, 11, 5, 6, 3));
         priorityProcesses.add(new Process(19, 18, 4, 2, 0, 0));
-        priorityProcesses.add(new Process(20, 19, 7, 3, 3, 1));
+        priorityProcesses.add(new Process(20, 19, 7, 3, 3, 1));*/
         
-        System.out.println("-----FCFS-----");
-        Algorithm schedule = new FCFS(fcfsProcesses);
-        schedule.Scheduling();
+        //System.out.println("-----FCFS-----");
+        //Algorithm schedule = new FCFS(fcfsProcesses);
+        //schedule.Scheduling();
         
-        System.out.println("-----RoundRobin-----");
-        schedule = new RoundRobin(rrProcesses, 4);
-        schedule.Scheduling();
+        //System.out.println("-----RoundRobin-----");
+        //schedule = new RoundRobin(rrProcesses, 4);
+        //schedule.Scheduling();
         
-        System.out.println("-----Priority-----");
+        /*System.out.println("-----Priority-----");
         schedule = new PriorityScheduling(priorityProcesses);
-        schedule.Scheduling();
+        schedule.Scheduling();*/
     }
 }
